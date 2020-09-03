03329e92ff9d96b65336ddc4566362d899ceaeb7
amirdy <amirdalili12@gmail.com>
2020-09-04 03:30:43 +0430

    Update myScripts.js

--- Web_App/static/myScripts.js
+++ Web_App/static/myScripts.js
@@ -1315,8 +1315,8 @@ $(document).ready(function () {
                 isImage = true;
                 $('#randomImage').addClass("d-none");
                 $('#cancelUpload').removeClass("d-none");
-		        $('#cancelUpload').removeClass('disabled');
-		        document.getElementById("cancelUpload").disabled = false;
+		$('#cancelUpload').removeClass('disabled');
+		document.getElementById("cancelUpload").disabled = false;
 
 
             }
@@ -1337,7 +1337,7 @@ $(document).ready(function () {
                             $('#ranImg').attr('src', addr);
                             $('#ranImg').attr('data-original-title', "New Uploaded Image");
                             $('#cancelUpload').addClass('disabled');
-				            document.getElementById("cancelUpload").disabled = true;
+			    document.getElementById("cancelUpload").disabled = true;
 
                         }
 
